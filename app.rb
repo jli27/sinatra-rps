eb425f4c75466171694e2b99151929a7625e601f
jli27 <96714100+jli27@users.noreply.github.com>
2025-01-24 20:48:03 +0000

    finished!

--- app.rb
+++ app.rb
@@ -1,9 +1,59 @@
 require "sinatra"
 require "sinatra/reloader"
 
+bank = Array.new
+bank.push("rock")
+bank.push("paper")
+bank.push("scissors")
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  @our_choice = "rock"
+  index = rand(0..2)
+  @random = bank.at(index)
+
+  if @our_choice == @random
+    @result = "We tied!</h2>"
+  elsif @random == "scissors"
+    @result = "We won!"
+  else
+    @result = "We lost!"
+  end
+
+  erb(:setup, { :layout => :wrapper })
+end
+
+get("/paper") do
+  @our_choice = "paper"
+  index = rand(0..2)
+  @random = bank.at(index)
+
+  if @our_choice == @random
+    @result = "We tied!</h2>"
+  elsif @random == "scissors"
+    @result = "We lost!"
+  else
+    @result = "We won!"
+  end
+
+  erb(:setup, { :layout => :wrapper })
+end
+
+get("/scissors") do
+  @our_choice = "scissors"
+  index = rand(0..2)
+  @random = bank.at(index)
+
+  if @our_choice == @random
+    @result = "We tied!</h2>"
+  elsif @random == "paper"
+    @result = "We won!"
+  else
+    @result = "We lost!"
+  end
+
+  erb(:setup, { :layout => :wrapper })
 end
